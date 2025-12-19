7a722f189db9a9c4c7577d336187391253e2ef2b
AKRKSMT0114 <225696801+AKRKSMT0114@users.noreply.github.com>
2025-12-19 14:33:08 +0900

    header.php style.css 追加

---
+++ header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta http-equiv="Content-type" content="text/html;charset=UTF-8" />
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <link rel="stylesheet" href="<?=get_template_directory_uri(); ?>/reset.css">
+    <link rel="stylesheet" href="<?=get_template_directory_uri(); ?>/style.css">
+    <?php wp_head(); ?>
+</head>
+
+<body>
+    <header class="header">
+        <h1 class="logo">
+            <div class="logo-name">白砂の丘市立学校給食センター</div>
+        </h1>
+        <ul class="menu-pc">
+            <li><a href="#わたしたちについて">わたしたちについて</a></li>
+            <li><a href="#給食ができるまで">給食ができるまで</a></li>
+            <li><a href="#きょうの献立">きょうの献立</a></li>
+            <li><a href="#おしらせ">おしらせ</a></li>
+            <li><a href="#お問い合わせ">お問い合わせ</a></li>
+        </ul>
+    </header>
