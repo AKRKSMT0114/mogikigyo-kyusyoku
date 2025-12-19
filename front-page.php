46aad456649c7f4c33cccd14054494f7b2d21491
AKRKSMT0114 <225696801+AKRKSMT0114@users.noreply.github.com>
2025-12-19 14:50:14 +0900

    front-page.php

---
+++ front-page.php
@@ -0,0 +1,12 @@
+<?php get_header() ?>
+<main>
+    <!-- fv -->
+    <section class="front-fv">
+        <img src="<?= get_template_directory_uri(); ?>mogikigyo-kyusyoku/img/top" alt="ファーストビュー">
+        <h2>
+            今日も、<span>笑顔</span>がひろがる給食を
+        </h2>
+    </section>
+    <!-- fv -->
+</main>
+<?php get_footer() ?>
