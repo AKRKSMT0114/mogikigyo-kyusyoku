f7e98abc99f80ef796e0924c3c4178b6f6917fb5
AKRKSMT0114 <225696801+AKRKSMT0114@users.noreply.github.com>
2025-12-20 22:46:35 +0900

    front-page.php style.css 追加

--- front-page.php
+++ front-page.php
@@ -10,8 +10,25 @@
     <!-- fv -->
     <!-- About -->
     <section class="front-about">
-        <h2 class="front-common__title">わたしたちについて</h2>
+        <h2 class="front-common-title">わたしたちについて</h2>
         <img src="<?= get_template_directory_uri() ?>/img/tuite.png" alt="わたしたちについて">
+    </section>
+    <section class="front-about">
+        <h2 class="front-common-title">給食ができるまで</h2>
+        <div class="process-container">
+            <div class="process-item">
+                <img src="<?= get_template_directory_uri() ?>/img/No1.png" alt="食材の受け取り">
+            </div>
+            <div class="arrow"></div>
+            <div class="process-item">
+                <img src="<?= get_template_directory_uri() ?>/img/No2.png" alt="洗浄">
+            </div>
+            <div class="arrow"></div>
+            <div class="process-item">
+                <img src="<?= get_template_directory_uri() ?>/img/No3.png" alt="調理">
+            </div>
+        </div>
+        <h3><a href="#" class="btn-box">給食ができるまで＞</a></h3>
     </section>
 </main>
 <?php get_footer() ?>
