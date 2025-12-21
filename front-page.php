9c4f92abb71e1c736768c802413e6a4cdfa9a667
AKRKSMT0114 <225696801+AKRKSMT0114@users.noreply.github.com>
2025-12-21 17:23:54 +0900

    front-page.php style.css 追加

--- front-page.php
+++ front-page.php
@@ -30,5 +30,39 @@
         </div>
         <h3><a href="#" class="btn-box">給食ができるまで＞</a></h3>
     </section>
+    <section class="front-about">
+        <h2 class="front-common-title">きょうの献立</h2>
+        <div class="today-menu">〇月〇日（金）献立</div>
+        <div class="menu-list">
+            <img src="<?= get_template_directory_uri() ?>/img/kondate.png" alt="献立">
+            <div class="menu-item">
+                <div class="menu-section">
+                    <ul>
+                        <li>ご飯</li>
+                        <li>牛乳</li>
+                        <li>井笠港産サーモンのメンチカツ</li>
+                        <li>具だくさん味噌汁</li>
+                        <li>ノンエッグマヨのポテトサラダ</li>
+                        <li>2世紀梨ゼリー</li>
+                        <div class="menu-section-2">
+                            <li>流星米</li>
+                            <li>牛乳</li>
+                            <li>井笠港産サーモン</li>
+                            <li>山大豚</li>
+                            <li>麒麟県産大根</li>
+                            <li>ジャガイモ</li>
+                            <li>2世紀ナシ</li>
+                            <span class="menu-ar">
+                                <p>牛乳</p>
+                                <p>※メンチカツの衣（米粉）</p>
+                                <p>揚げ油（こめ油）</p>
+                            </span>
+                        </div>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        </div>
+    </section>
 </main>
 <?php get_footer() ?>
