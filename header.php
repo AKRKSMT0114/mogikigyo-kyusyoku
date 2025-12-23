f8b2c192da68441895b562b263dc9bbee880c2e7
AKRKSMT0114 <225696801+AKRKSMT0114@users.noreply.github.com>
2025-12-23 17:35:01 +0900

    header.php style.css 追加

--- header.php
+++ header.php
@@ -4,8 +4,6 @@
 <head>
     <meta http-equiv="Content-type" content="text/html;charset=UTF-8" />
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
-    <link rel="stylesheet" href="<?=get_template_directory_uri(); ?>/reset.css">
-    <link rel="stylesheet" href="<?=get_template_directory_uri(); ?>/style.css">
     <?php wp_head(); ?>
 </head>
 
@@ -21,4 +19,18 @@
             <li><a href="#おしらせ">おしらせ</a></li>
             <li><a href="#お問い合わせ">お問い合わせ</a></li>
         </ul>
+
+        <!-- 追加 -->
+        <div id="btn" class="hamburger__btn">
+            <i class="fas fa-bars"></i>
+        </div>
+        <ul id="menu" class="menu-sp">
+            <li><a href="#わたしたちについて">わたしたちについて</a></li>
+            <li><a href="#給食ができるまで">給食ができるまで</a></li>
+            <li><a href="#きょうの献立">きょうの献立</a></li>
+            <li><a href="#おしらせ">おしらせ</a></li>
+            <li><a href="#お問い合わせ">お問い合わせ</a></li>
+        </ul>
+        <div id="modal" class="modal-sp"></div>
+        <!-- 追加 -->
     </header>
