a4556ff0c70ff8a52a9b73a2890453e9d1f2b339
AKRKSMT0114 <225696801+AKRKSMT0114@users.noreply.github.com>
2025-12-24 11:11:48 +0900

    hamburger.js footer.php 追加

--- footer.php
+++ footer.php
@@ -1,6 +1,8 @@
 <footer class="footer">
     <p><small>Copyright © 麒麟県給食会, All rights reserved.</small></p>
+    <script src="<?= get_template_directory_uri(); ?>/hamburger.js"></script>
 </footer>
 <?php wp_footer(); ?>
 </body>
+
 </html>
