effe88417ca89b56260557d53a6401340e4b61b2
AKRKSMT0114 <225696801+AKRKSMT0114@users.noreply.github.com>
2025-12-28 15:03:06 +0900

    sunamaru.php 新規追加　style.css 追加

---
+++ sunamaru.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Template Name: 紹介ページテンプレート
+ */
+get_header(); ?>
+
+<main class="about-page">
+    <div class="container">
+        <h2 class="page-title">すなまるについて</h2>
+        <div class="white-card">
+            <div class="card-content">
+                <div class="card-image">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/sunamaru-page.png" alt="すなまる">
+                </div>
+                <div class="card-text">
+                    <h3 class="char-name">すなまる</h3>
+                    <p>テキストテキストテキストテキスト<br>
+                        テキストテキストテキストテキスト<br>
+                        テキストテキストテキストテキスト<br>
+                        テキストテキストテキストテキスト</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+<?php get_footer(); ?>
