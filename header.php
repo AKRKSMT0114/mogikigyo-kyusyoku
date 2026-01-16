9c5ac62db07af731e1d9e3b688de9d55c3242664
AKRKSMT0114 <225696801+AKRKSMT0114@users.noreply.github.com>
2026-01-16 20:26:21 +0900

    header.php 変更

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 <head>
     <meta http-equiv="Content-type" content="text/html;charset=UTF-8" />
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
-    <link rel="stylesheet" 	href="https://use.fontawesome.com/releases/v5.15.1/css/all.css">    <?php wp_head(); ?>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css"> <?php wp_head(); ?>
 </head>
 
 <body>
@@ -13,7 +13,7 @@
             <div class="logo-name">白砂の丘市立学校給食センター</div>
         </h1>
         <ul class="menu-pc">
-            <li><a href="https://siraoka-kyusyoku.punyu.jp/%e3%82%8f%e3%81%9f%e3%81%97%e3%81%9f%e3%81%a1%e3%81%ab%e3%81%a4%e3%81%84%e3%81%a6/">わたしたちについて</a></li>
+            <li><a href="<?php echo home_url('/tuite_page/'); ?>">わたしたちについて＞</a></li>
             <li><a href="#給食ができるまで">給食ができるまで</a></li>
             <li><a href="#きょうの献立">きょうの献立</a></li>
             <li><a href="#おしらせ">おしらせ</a></li>
@@ -23,7 +23,7 @@
             <i class="fas fa-bars"></i>
         </div>
         <ul id="menu" class="menu-sp">
-            <li><a href="https://siraoka-kyusyoku.punyu.jp/%e3%82%8f%e3%81%9f%e3%81%97%e3%81%9f%e3%81%a1%e3%81%ab%e3%81%a4%e3%81%84%e3%81%a6/">わたしたちについて</a></li>
+            <li><a href="<?php echo home_url('/tuite_page/'); ?>">わたしたちについて</a></li>
             <li><a href="#給食ができるまで">給食ができるまで</a></li>
             <li><a href="#きょうの献立">きょうの献立</a></li>
             <li><a href="#おしらせ">おしらせ</a></li>
