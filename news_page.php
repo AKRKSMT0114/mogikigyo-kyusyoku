7f9030c63ad4faecd4822f1b7f470fc361ea3c7a
AKRKSMT0114 <225696801+AKRKSMT0114@users.noreply.github.com>
2026-01-21 21:40:20 +0900

    news_page.php 追加

--- news_page.php
+++ news_page.php
@@ -3,8 +3,12 @@
 Template Name: news(ニュース一覧)
 */
 ?>
- 
+
 <!-- 追加 -->
 <?php get_header() ?>
 <h2> ニュース一覧 </h2>
+<div class="history-button">
+    <a href="<?php echo esc_url(home_url('/')); ?>" class="btn-white">トップへ戻る ＞</a>
+</div>
+
 <?php get_footer() ?>
